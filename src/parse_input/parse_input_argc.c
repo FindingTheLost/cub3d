712ecd856886af764f15c0bb0e31d57ca6fff1d2
Finding The Lost <findingthelost1999@gmail.com>
2026-05-26 00:32:00 +0100

    Fixed a few typos with the help screen and extended the message given in case no arguments are provided when launching cub3D.

--- src/parse_input/parse_input_argc.c
+++ src/parse_input/parse_input_argc.c
@@ -6,7 +6,7 @@
 /*   By: pde-alme <pde-alme@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 18:09:20 by pde-alme          #+#    #+#             */
-/*   Updated: 2026/05/11 15:59:44 by pde-alme         ###   ########.fr       */
+/*   Updated: 2026/05/26 00:14:22 by pde-alme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ int	parse_input_argc(int argc)
 	{
 		ft_printf(RED_LIGH "Error\n" DEF);
 		if (argc < 2)
-			ft_printf("Too few arguments were given!\n");
+		{
+			ft_printf("Too few arguments were given. For help, append \"-h\"");
+			ft_printf(" or \"--help\" when launching the program!\n");
+		}
 		else
 			ft_printf("Too many arguments were given!\n");
 		return (false);
--- src/parse_input/parse_input_help.c
+++ src/parse_input/parse_input_help.c
@@ -6,7 +6,7 @@
 /*   By: pde-alme <pde-alme@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 23:46:52 by pde-alme          #+#    #+#             */
-/*   Updated: 2026/05/01 03:22:14 by pde-alme         ###   ########.fr       */
+/*   Updated: 2026/05/26 00:22:43 by pde-alme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 static void	show_help_map(void)
 {
 	ft_printf(YEL_LINE YEL_BOLD "Map content:\n" DEF);
-	ft_printf(" - A " RED_BOLD "1" DEF " represents a wall;\n");
-	ft_printf(" - A " RED_BOLD "0" DEF " represents a traversable floor");
-	ft_printf(" surface;\n");
+	ft_printf(" - A character " RED_BOLD "1" DEF " represents a wall;\n");
+	ft_printf(" - A character " RED_BOLD "0" DEF " represents a traversable");
+	ft_printf(" floor surface;\n");
 	ft_printf(" - The characters " RED_BOLD "N" DEF "," RED_BOLD " S" DEF);
-	ft_printf(", " RED_BOLD "E" DEF ", " RED_BOLD "W" DEF " represent ");
+	ft_printf(", " RED_BOLD "W" DEF ", " RED_BOLD "E" DEF " represent ");
 	ft_printf("a player and its orientation when initializing the map.\n");
 	ft_printf(" - A map can have any shape and size, as long as it is ");
 	ft_printf("properly surrounded by walls.\n");
-	ft_printf(" - A map cannot possess an empty line, however, ");
+	ft_printf(" - A map cannot possess an empty line mid map, however, ");
 	ft_printf(RED_BOLD "spaces" DEF " are allowed.\n\n");
 }
 
@@ -34,15 +34,15 @@ static void	show_help_elements(void)
 	ft_printf(".xpm" DEF " texture file;\n");
 	ft_printf(" - A " YEL_BOLD "SO" DEF " element, followed by a " RED_BOLD);
 	ft_printf(".xpm" DEF " texture file;\n");
-	ft_printf(" - A " YEL_BOLD "EA" DEF " element, followed by a " RED_BOLD);
-	ft_printf(".xpm" DEF " texture file;\n");
 	ft_printf(" - A " YEL_BOLD "WE" DEF " element, followed by a " RED_BOLD);
+	ft_printf(".xpm" DEF " texture file;\n");
+	ft_printf(" - A " YEL_BOLD "EA" DEF " element, followed by a " RED_BOLD);
 	ft_printf(".xpm" DEF " texture file.\n\n");
 	ft_printf(YEL_LINE YEL_BOLD "Ceiling and floor colors:\n" DEF);
-	ft_printf(" - A " YEL_BOLD "F" DEF " element, followed by " RED_BOLD "3");
+	ft_printf(" - A " YEL_BOLD "C" DEF " element, followed by " RED_BOLD "3");
 	ft_printf(DEF " comma separated values ranging from " RED_BOLD "0" DEF);
 	ft_printf(DEF " - " RED_BOLD "255" DEF ";\n");
-	ft_printf(" - A " YEL_BOLD "C" DEF " element, followed by " RED_BOLD "3");
+	ft_printf(" - A " YEL_BOLD "F" DEF " element, followed by " RED_BOLD "3");
 	ft_printf(DEF " comma separated values ranging from " RED_BOLD "0" DEF);
 	ft_printf(DEF " - " RED_BOLD "255" DEF ".\n\n");
 }
@@ -53,7 +53,7 @@ static void	show_help_usage(void)
 	ft_printf("To launch it, input a " RED_BOLD ".cub" DEF " file as launch ");
 	ft_printf("parameter:\n");
 	ft_printf(YEL_BOLD "./cub3D [map_name].cub\n\n" DEF);
-	ft_printf("A " RED_BOLD ".cub" DEF " file can be composed by the ");
+	ft_printf("A " RED_BOLD ".cub" DEF " file can be composed of the ");
 	ft_printf("following elements in " RED_BOLD "any" DEF " given order:\n\n");
 }
 
